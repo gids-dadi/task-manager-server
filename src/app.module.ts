2b93022cc5f95057a8b5d66e472d13ac68b8a79f
Tobi Samuel <ufomatobi@gmail.com>
2025-06-09 16:07:22 +0100

    Add ed task dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ControllerModule } from './controller/controller.module';
 import { ServiceModule } from './service/service.module';
+import { TasksModule } from './tasks/tasks.module';
+import { TaskController } from './task/task.controller';
 
 @Module({
-  imports: [UsersModule, AuthModule, ControllerModule, ServiceModule],
-  controllers: [AppController],
+  imports: [UsersModule, AuthModule, ControllerModule, ServiceModule, TasksModule],
+  controllers: [AppController, TaskController],
   providers: [AppService],
 })
 export class AppModule {}
