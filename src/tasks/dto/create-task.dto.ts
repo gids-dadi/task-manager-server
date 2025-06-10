cbd50eb65336cee6f0702be6039fc09eba70f26d
Tobi Samuel <ufomatobi@gmail.com>
2025-06-10 04:03:11 +0100

    Added status to task

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,9 +1,16 @@
 import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import e from 'express';
 
 export enum Priority {
   LOW = 'low',
   MEDIUM = 'medium',
   HIGH = 'high',
+}
+
+export enum TaskStatus {
+  TODO = 'todo',
+  IN_PROGRESS = 'in-progress',
+  COMPLETED = 'completed',
 }
 
 export class CreateTaskDto {
@@ -16,6 +23,9 @@ export class CreateTaskDto {
 
   @IsEnum(Priority, { message: 'Priority must be low, medium, or high' })
   priority: Priority;
+  
+  @IsEnum(TaskStatus, { message: 'Status must be todo, in-progress, or completed' })
+  status: TaskStatus;
 
   @IsNotEmpty({ message: 'Due date is required' })
   dueDate: Date;
